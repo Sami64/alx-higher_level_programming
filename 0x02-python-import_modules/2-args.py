d9ebcbd5592d4a49f140ee526c33745229760fb1
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-03-15 04:05:21 +0000

    args 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     total = len(sys.argv) - 1
     if total == 0:
         print("0 arguments.")
@@ -10,3 +11,4 @@ if __name__ == "__main__":
         print("{} arguments:".format(total))
     for i in range(total):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
+ 
