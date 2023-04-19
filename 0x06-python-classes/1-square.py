10ddd302a3801e26b2e35cc7b067222f373baa06
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-04-19 13:25:02 +0000

    square class size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is a square module"""
+
+
+class Square:
+    """A class Square that defines a square"""
+
+    def __init__(self, size):
+        """Initialize the class"""
+        self.__size = size
