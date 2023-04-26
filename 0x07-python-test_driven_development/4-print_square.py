7d2470a5b60791b2271b0dd2cf97ef7d3da6a8d2
Samuel Kow Paintsil <smashbros54@gmail.com>
2023-04-26 10:27:09 +0000

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is a square module"""
+
+
+def print_square(size):
+    """ This prints a square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    if type(size) is float:
+        raise TypeError("size must be an integer")
+
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
